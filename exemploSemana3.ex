41947d1099c8edf868dd77d092511d7f26fb2cb3
Bruno Silveira <contato.brunosilveira@gmail.com>
2023-11-09 21:29:00 -0300

    exercicio 1 da lista 2

---
+++ exemploSemana3.ex
@@ -0,0 +1,14 @@
+defmodule Vendas do
+def vendas(0), do: 33
+def vendas(1), do: 22
+def vendas(2), do: 18
+def vendas(3), do: 0
+def vendas(4), do: 66
+def vendas(_n), do: 22
+def venda_total(0) do
+ vendas(0)
+end
+def venda_total(n) do
+ vendas(n) + venda_total(n-1)
+end
+end
---
+++ lista2/ex1.ex
@@ -0,0 +1,8 @@
+defmodule Maior do
+    def maxi(a,b) do
+        cond do
+            a >= b -> a
+            a <= b -> b
+        end
+    end
+end
