c7da8c6cb3b7d935f5447f13523b89dbf8f49e8b
Bruno Silveira <contato.brunosilveira@gmail.com>
2023-11-18 10:33:23 -0300

    Produto

--- lista3/exercicios_lista3.ex
+++ lista3/exercicios_lista3.ex
@@ -3,10 +3,20 @@ defmodule TerceiraLista do
     []
   end
 
+  #Exercício 1
   def mult_dois_lista([head | tail]) do
     [head * 2 | mult_dois_lista(tail)]
   end
 
+  #Exercício 2
   def tamanho([]), do: 0
-  def tamanho([_ | tail]), do: 1 + tamanho(tail)
+  def tamanho([_ | tail]) do
+     1 + tamanho(tail)
+  end
+
+  #Exercício 3
+  def produto([]), do: 1
+  def produto([head|tail]) do
+    head * produto(tail)
+  end
 end
